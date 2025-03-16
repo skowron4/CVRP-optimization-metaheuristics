246463df336b23ee8eed4562bfc71fe36e16633a
adamr <adam.rud02@gmail.com>
2025-03-16 21:47:19 +0100

    Implement Individual methods

---
+++ src/Individual.cpp
@@ -0,0 +1,31 @@
+#include "Individual.h"
+
+ostream &operator<<(ostream &os, const Individual &individual) {
+    for (int gene: individual.genotype) os << setw(4) << gene;
+    return os;
+}
+
+bool Individual::operator>(const Individual &other) const {
+    return problem.isFirstFitnessBetter(getFitness(), other.getFitness());
+}
+
+bool Individual::operator<(const Individual &other) const {
+    return problem.isFirstFitnessBetter(other.getFitness(), getFitness());
+}
+
+bool Individual::operator==(const Individual &other) const {
+    return getFitness() == other.getFitness() && genotype == other.genotype;
+}
+
+int Individual::getFitness() const {
+    if (!is_evaluated) {
+        fitness = problem.evaluateGenotype(genotype);
+        is_evaluated = true;
+    }
+    return fitness;
+}
+
+void Individual::mutate(const Mutation &mutation) {
+    mutation.mutate(genotype);
+    is_evaluated = false;
+}
