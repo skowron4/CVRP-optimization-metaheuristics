386e0c953bf8e7841d7c274c42a9917ef9182cb3
skowron4 <adam.skowron44@gmail.com>
2025-03-17 03:35:16 +0100

    Loader implementation

--- include/Loader.h
+++ include/Loader.h
@@ -7,8 +7,6 @@
 
 using namespace std;
 
-class Problem;
-
 namespace Loader {
     struct City {
         int id, x, y;
@@ -25,7 +23,25 @@ namespace Loader {
         int dimension, capacity, depot;
     };
 
-    optional<Problem> loadProblemFromFile(const string &filepath);
+    /* I thought about it a lot and came to the conclusion that we should return only data struct
+     * Why? Because solid and circular dependencies.
+     * First: we should read only the data and return it
+     * Second: We should pass it as a parameter to the problem constructor, for example in main
+     */
+
+    optional<Loader::Data> loadProblemFromFile(const string &filepath);
+
+    void parseKey(std::istringstream &iss, int &value);
+
+    void parseCity(const std::string &line, Data &data);
+
+    void parseDemand(const std::string &line, Data &data);
+
+    void parseDepot(const std::string &line, Data &data);
+
+    bool validateData(const Data &data) {
+        return data.cities.size() == data.dimension || data.demands.size() == data.dimension;
+    }
 }
 
 #endif //LOADER_H
---
+++ src/Loader.cpp
@@ -0,0 +1,88 @@
+#include <fstream>
+#include <sstream>
+#include "Loader.h"
+#include "iostream"
+
+using namespace std;
+
+optional<Loader::Data> Loader::loadProblemFromFile(const string &filepath) {
+    std::ifstream file(filepath);
+
+    if (!file.is_open()) {
+        std::cerr << "Error: Cannot open file" << filepath << "\n";
+        return nullopt;
+    }
+
+    Data data;
+    std::string line;
+    bool inCitySection{false}, inDemandSection{false}, inDepotSection{false};
+
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string key;
+        iss >> key;
+
+        if (key == "DIMENSION") {
+            parseKey(iss, data.dimension);
+        } else if (key == "CAPACITY") {
+            parseKey(iss, data.capacity);
+        } else if (key == "NODE_COORD_SECTION") {
+            inCitySection = true;
+            inDemandSection = false;
+            inDepotSection = false;
+            continue;
+        } else if (key == "DEMAND_SECTION") {
+            inCitySection = false;
+            inDemandSection = true;
+            inDepotSection = false;
+            continue;
+        } else if (key == "DEPOT_SECTION") {
+            inCitySection = false;
+            inDemandSection = false;
+            inDepotSection = true;
+            continue;
+        } else if (key == "EOF") {
+            break;
+        }
+
+        if (inCitySection) {
+            parseCity(line, data);
+        } else if (inDemandSection) {
+            parseDemand(line, data);
+        } else if(inDepotSection) {
+            parseDepot(line, data);
+            break;
+        }
+    }
+
+    if (validateData(data)) {
+        std::cerr << "Error: numbers of cities doesn't fit\n";
+        return nullopt;
+    }
+
+    return make_optional<Data>(data);
+}
+
+void Loader::parseKey(std::istringstream &iss, int &value) {
+    std::string temp;
+    iss >> temp >> value;
+}
+
+void Loader::parseCity(const std::string &line, Data &data) {
+    City city{};
+    std::istringstream iss(line);
+    iss >> city.id >> city.x >> city.y;
+    data.cities.push_back(city);
+}
+
+void Loader::parseDemand(const std::string &line, Data &data) {
+    Demand demand{};
+    std::istringstream iss(line);
+    iss >> demand.city_id >> demand.value;
+    data.demands.push_back(demand);
+}
+
+void Loader::parseDepot(const std::string &line, Data &data) {
+    std::istringstream iss(line);
+    iss >> data.depot;
+}
