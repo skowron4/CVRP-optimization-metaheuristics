67b6f714f22a2e558ef3bc48fa5a0fcd216b0d5b
adamr <adam.rud02@gmail.com>
2025-03-17 11:57:15 +0100

    Implement Problem methods and adjust few methods' signature

--- include/Problem.h
+++ include/Problem.h
@@ -1,8 +1,10 @@
 #ifndef PROBLEMCVRP_H
 #define PROBLEMCVRP_H
 
+#include "Individual.h"
 #include "Loader.h"
 #include <vector>
+#include <random>
 
 using namespace std;
 
@@ -17,18 +19,25 @@ private:
 
     void computeDistanceMatrix();
 
-    Individual createGreedyIndividual(int startingCity) const;
+    Individual createGreedyIndividual(int startingCity);
 
 public:
-    explicit Problem(const Loader::Data data) : data(data) {};
+    explicit Problem(const Loader::Data& data) : data(data) {
+        genotype_size = 2 * data.dimension - 3;
+        cities_count = data.dimension - 1;
+        depots_count = cities_count - 1;
+        computeDistanceMatrix();
+    };
 
     int evaluateGenotype(const vector<int> &genotype) const;
 
-    Individual createRandomIndividual(mt19937 &random_engine) const;
+    Individual createRandomIndividual(mt19937 &random_engine);
 
-    vector<Individual> createGreedyIndividuals() const;
+    vector<Individual> createGreedyIndividuals();
 
     bool isFirstFitnessBetter(int firstFitness, int secondFitness) const;
+
+    void printData() const;
 
     void printDistanceMatrix() const;
 };
