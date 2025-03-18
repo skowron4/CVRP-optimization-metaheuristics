c38094bb0e15b4cae4d1eda7e432386117e90b3b
adamr <adam.rud02@gmail.com>
2025-03-19 00:28:34 +0100

    Remove redundant class field and method and adjust imports

--- include/Problem.h
+++ include/Problem.h
@@ -1,7 +1,6 @@
 #ifndef PROBLEMCVRP_H
 #define PROBLEMCVRP_H
 
-#include "Individual.h"
 #include "Loader.h"
 #include <vector>
 #include <random>
@@ -15,7 +14,7 @@ private:
     Loader::Data data;
 
     vector<vector<int>> distance_matrix;
-    int genotype_size, cities_count, depots_count;
+    int genotype_size, cities_count;
 
     void computeDistanceMatrix();
 
@@ -25,7 +24,6 @@ public:
     explicit Problem(const Loader::Data& data) : data(data) {
         genotype_size = 2 * data.dimension - 3;
         cities_count = data.dimension - 1;
-        depots_count = cities_count - 1;
         computeDistanceMatrix();
     };
 
@@ -34,8 +32,6 @@ public:
     Individual createRandomIndividual(mt19937 &random_engine);
 
     vector<Individual> createGreedyIndividuals();
-
-    bool isFirstFitnessBetter(int firstFitness, int secondFitness) const;
 
     void printData() const;
 
