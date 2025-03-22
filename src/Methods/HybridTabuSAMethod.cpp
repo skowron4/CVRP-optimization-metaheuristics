a9c1c2b98e3c361644b0de125289a3986efd400e
skowron4 <adam.skowron44@gmail.com>
2025-03-22 19:40:31 +0100

    Moving methods to parent class for better code organization Implementation of annealing algorithm Improvement of algorithm step in tabusearch

--- src/Methods/HybridTabuSAMethod.cpp
+++ src/Methods/HybridTabuSAMethod.cpp
@@ -1,5 +1,15 @@
 #include "Methods.h"
 
+void HybridTabuSAMethod::algorithmStep(Individual &currentIndividual,
+                                             Individual &bestIndividual,
+                                             vector<Individual> &neighborhood) {
+
+}
+
+Individual* HybridTabuSAMethod::findBestIndividual(vector<Individual> &neighborhood) {
+
+}
+
 Individual HybridTabuSAMethod::run() {
 
 }
