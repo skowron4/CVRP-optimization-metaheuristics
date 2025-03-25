d9877dcd3367b934b85b7c9dec3ec47183fe5e9c
adamr <adam.rud02@gmail.com>
2025-03-26 00:21:36 +0100

    Add SingleChartProcessor class for plotting fitness statistics from CSV data

---
+++ scripts/single_chart_processor.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from csv_processor import CSVProcessor
+
+
+class SingleChartProcessor(CSVProcessor):
+    def _extract_data(self, df):
+        iterations = range(len(df))
+        best, worst, avg = df['best'], df['worst'], df['avg']
+        return iterations, best, worst, avg
+
+    def _plot_chart(self, data, output_filepath):
+        iterations, best, worst, avg = data
+
+        best_overall = best.min()
+        cumulative_best = best.cummin()
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(iterations, best, label=f'Best (Overall: {best_overall})', color='blue')
+        plt.plot(iterations, worst, label='Worst', color='red')
+        plt.plot(iterations, avg, label='Average', color='green')
+        plt.plot(iterations, cumulative_best, label='Best Currently Found', color='purple')
+
+        plt.xlabel('Iterations')
+        plt.ylabel('Fitness')
+        plt.title('Fitness Statistics Over Iterations')
+        plt.legend()
+        plt.savefig(output_filepath)
+        plt.close()
