eae2cb991a0d1f3a9e17a9cf353754cdc0d1013b
arud <adam.rud02@gmail.com>
2025-03-27 23:09:14 +0100

    Add method to concurrently compute single method runs and update MethodRunner.h accordingly.

--- include/MethodRunner.h
+++ include/MethodRunner.h
@@ -1,7 +1,3 @@
-//
-// Created by User on 26.03.2025.
-//
-
 #ifndef CVRP_OPTIMIZATION_METAHEURISTICS_METHODRUNNER_H
 #define CVRP_OPTIMIZATION_METAHEURISTICS_METHODRUNNER_H
 
@@ -11,31 +7,31 @@
 
 using json = nlohmann::json;
 
-
 class MethodRunner {
 public:
-    MethodRunner(Problem& problem,
-                 const json& config,
-                 std::unordered_map<std::string, std::unique_ptr<Method>>& methods) :
-                 problem(problem),
-                 config(config),
-                 methods(methods) {}
+    MethodRunner(Problem &problem,
+                 const json &config,
+                 std::unordered_map<std::string, std::unique_ptr<Method>> &methods) :
+            problem(problem),
+            config(config),
+            methods(methods) {}
 
     void runMethods();
 
 private:
-    Problem& problem;
-    const json& config;
-    std::unordered_map<std::string, std::unique_ptr<Method>>& methods;
+    Problem &problem;
+    const json &config;
+    std::unordered_map<std::string, std::unique_ptr<Method>> &methods;
 
-    void runBoxPlotMethods(const json& boxPlotConfig);
+    void runBoxPlotMethods(const json &boxPlotConfig);
 
-    void runSinglePlotMethods(const json& singlePlotConfig);
+    void runSinglePlotMethods(const json &singlePlotConfig);
 
-    std::vector<std::string> extractMethodsFromJson(const json& config, const std::string& key);
+    std::vector<std::string> extractMethodsFromJson(const json &config, const std::string &key);
 
-    int extractIterationsFromJson(const json& config, const std::string& key);
+    int extractIterationsFromJson(const json &config, const std::string &key);
+
+    vector<Method *> getSelectedMethods(const json &config, const string &key);
 };
-
 
 #endif //CVRP_OPTIMIZATION_METAHEURISTICS_METHODRUNNER_H
