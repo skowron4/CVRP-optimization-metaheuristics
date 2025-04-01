fa1c62b1c486ffa88c8cbf0ce16b91667c85e087
arud <adam.rud02@gmail.com>
2025-04-01 12:03:43 +0200

    Adjust mutations

--- src/Mutations.cpp
+++ src/Mutations.cpp
@@ -5,28 +5,30 @@
 void SingleSwapMutation::mutate(vector<int> &genotype) {
     if (genotype.size() < 2) return;
 
-    vector<int> chosen(2);
+    uniform_int_distribution<int> int_dist(0, genotype.size() - 1);
+    int left = int_dist(random_engine);
+    int right;
 
-    do sample(indices.begin(), indices.end(), chosen.begin(), 2, random_engine);
-    while (genotype[chosen[0]] == genotype[chosen[1]]);
+    // continue IF (genotype[left] == 0 and genotype[right]) == 0 OR (right == left)
+    do right = int_dist(random_engine);
+    while (genotype[left] == genotype[right]);
 
-    swap(genotype[chosen[0]], genotype[chosen[1]]);
+    swap(genotype[left], genotype[right]);
 }
 
 void InversionMutation::mutate(vector<int> &genotype) {
     if (genotype.size() < 2) return;
 
-    int left, right;
-    std::uniform_int_distribution<int> int_dist(0, genotype.size() - 1);
-    left = int_dist(random_engine);
+    uniform_int_distribution<int> int_dist(0, genotype.size() - 1);
+    int left = int_dist(random_engine);
+    int right;
 
     do {
         right = int_dist(random_engine);
         if (left > right) swap(left, right);
     } while (
-            (genotype[left] == 0 && genotype[right] == 0) ||
-            ((left > 0 && genotype[left - 1] == 0) && (right < genotype.size() - 1 && genotype[right + 1] == 0)) ||
-            left == right);
+            genotype[left] == genotype[right] ||
+            (left > 0 && genotype[left - 1] == 0) && (right < genotype.size() - 1 && genotype[right + 1] == 0));
 
     reverse(genotype.begin() + left, genotype.begin() + right + 1);
 }
