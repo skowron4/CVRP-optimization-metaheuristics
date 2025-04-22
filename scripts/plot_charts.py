4e46d0b52f8a1cd1e5154d619779314ee666206f
arud <adam.rud02@gmail.com>
2025-04-22 20:28:25 +0200

    Refactor chart processing functions
    - accept dynamic location
    - improve C++ program execution handling
    - remove C++ program compilation function

--- scripts/plot_charts.py
+++ scripts/plot_charts.py
@@ -5,96 +5,77 @@ from concurrent.futures import ProcessPoolExecutor
 from csv_processors.box_chart_processor import BoxChartProcessor
 from csv_processors.single_chart_processor import SingleChartProcessor
 
-
 # Use non-interactive backend for matplotlib
 import matplotlib
 matplotlib.use('agg')
 
 
-def process_single_chart():
-    single_chart_processor = SingleChartProcessor('../data/results/single/', '../data/charts/single/')
-    single_chart_processor.process_csv_files()
+def process_single_chart(location):
+    src = os.path.join(location, 'results', 'single')
+    tgt = os.path.join(location, 'results', 'charts', 'single')
+    SingleChartProcessor(src, tgt).process_csv_files()
 
 
-def process_box_chart():
-    box_chart_processor = BoxChartProcessor('../data/results/box/', '../data/charts/box/')
-    box_chart_processor.process_csv_files()
+def process_box_chart(location):
+    src = os.path.join(location, 'results', 'box')
+    tgt = os.path.join(location, 'results', 'charts', 'box')
+    BoxChartProcessor(src, tgt).process_csv_files()
 
 
-def compile_cpp_program():
-    """Compiles the program using CMake."""
-
-    build_dir = r"C:\Users\User\CLionProjects\CVRP-optimization-metaheuristics\cmake-build-debug"
-
-    cmake_path = r"C:\Program Files\JetBrains\CLion 2023.3.4\bin\cmake\win\x64\bin"
-
-    if not os.path.exists(build_dir):
-        os.makedirs(build_dir)
-
+def start_cpp_program(exe_path, config_file, data_file, dest_dir):
+    cmd = [exe_path, config_file, data_file, dest_dir]
     try:
-        print("Running CMake configuration...")
-        result = subprocess.run([cmake_path, ".."], check=True, text=True, capture_output=True, cwd=build_dir)
-        print(result.stdout)
-
-        print("Building the project...")
-        result = subprocess.run([cmake_path, "--build", "."], check=True, text=True, capture_output=True, cwd=build_dir)
-        print(result.stdout)
-
-        print("Compilation completed successfully!")
-
-    except subprocess.CalledProcessError as e:
-        print("Error during compilation!")
-        print("STDOUT:", e.stdout)
-        print("STDERR:", e.stderr)
-        exit(1)
-
-
-def start_cpp_program(config_file, data_file):
-    """Runs a C++ program with a config file"""
-    cpp_program_path = '../cmake-build-debug/CVRP_optimization_metaheuristics.exe'
-    mingw_dll_path = "C://Program Files//JetBrains//CLion 2023.3.4//bin//mingw//bin"
-    env = os.environ.copy()
-    env["PATH"] = mingw_dll_path + ";" + env["PATH"]
-
-    try:
-        result = subprocess.run([cpp_program_path, config_file, data_file],
-                                env=env,
+        result = subprocess.run(cmd,
                                 check=True,
                                 text=True,
                                 capture_output=True)
-
-        print("Program output:", result.stdout)
-        print("Program error (if any):", result.stderr)
-
+        print("C++ stdout:\n", result.stdout)
+        print("C++ stderr:\n", result.stderr)
     except subprocess.CalledProcessError as e:
-        print(f"Error during execution: {e}")
-        print("STDOUT:", e.stdout)
-        print("STDERR:", e.stderr)
+        print(f"[ERROR] C++ exited {e.returncode}")
+        print("Stdout:", e.stdout)
+        print("Stderr:", e.stderr)
+        raise
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Process and plot CSV files.')
-    parser.add_argument('--charts', type=str, default='single,box', help='Comma-separated list of charts to process (single, box)')
-    parser.add_argument('config_file', type=str, help='Path to the C++ config file')
-    parser.add_argument('data_file', type=str, help='Path to the data file')
+    parser = argparse.ArgumentParser(description='(Re)run C++ and/or generate charts from CSV results.')
+    parser.add_argument('--exe',
+                        type=str,
+                        required=False,
+                        help='Path to C++ binary in case of additionally running it in advance. Defaults to None.')
+    parser.add_argument('--charts',
+                        type=str,
+                        default='single,box',
+                        help='Comma-separated charts to generate (single,box). Defaults to all.')
+    parser.add_argument('--location', "--loc",
+                        type=str,
+                        default='./',
+                        help="Location of 'results' folder with at least one subfolder: \
+                              'results/single' or 'results/box', created previously by the C++ program. \
+                              Charts will be saved in 'charts/single' or 'charts/box' subfolders. \
+                              Defaults to the current working directory ('./').")
+    parser.add_argument('config',
+                        type=str,
+                        help='Path to the config file for the C++ program')
+    parser.add_argument('data',
+                        type=str,
+                        help='Path to the data file for the C++ program')
     args = parser.parse_args()
 
-    charts = args.charts.split(',')
+    location = os.path.abspath(args.location)
 
-    # compile_cpp_program()
+    if args.exe:
+        print(f"Running C++ program with config: {args.config} and data: {args.data}")
+        print(f"Results will be saved in: {location}")
+        start_cpp_program(args.exe, args.config, args.data, location)
 
-    start_cpp_program(args.config_file, args.data_file)
-
+    charts = {chart.strip().lower() for chart in args.charts.split(',')}
     with ProcessPoolExecutor() as executor:
-        futures = []
         if 'single' in charts:
-            futures.append(executor.submit(process_single_chart))
+            executor.submit(process_single_chart, location)
         if 'box' in charts:
-            futures.append(executor.submit(process_box_chart))
-
-        for future in futures:
-            future.result()
-
+            executor.submit(process_box_chart, location)
 
 if __name__ == '__main__':
     main()
